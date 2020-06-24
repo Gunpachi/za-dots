fc0365d37b3d67f02df60adc6c8560cc95827c99
Gunpachi <37960846+Gunpachi@users.noreply.github.com>
2020-06-24 18:36:12 +0000

    Add files via upload

---
+++ installers/Arch_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+
+#Update the system
+sudo pacman -Syyu
+
+#installing pikaur
+
+#Installing necessary apps
+
+
+
+
+
+#Git clone the dotfiles repo
+
+#copying WM configs
+
+#copying other app configs
+
+########### E N D ###########
