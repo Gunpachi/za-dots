e73c074cba4c71ad117687360b03f712264fba7a
Gunpachi <37960846+Gunpachi@users.noreply.github.com>
2020-07-04 05:26:01 +0000

    added some basic stuff for a clean openbox setup

--- Scripts/OS_Setup/Void_setup.sh
+++ Scripts/OS_Setup/Void_setup.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
 
 ### Void linux setup ####
+sudo sbps-install -Su
+sudo xbps-install -Su
 
-
+sudo xbps-install -S void-repo-nonfree 
+sudo xbps-install -S elogind openbox obconf lxappearance tint2 picom xorg-minimal xorg-input-drivers xf86-video-intel pcmanfm xorg-video-drivers setxkbmap xauth font-misc-misc terminus-font dejavu-fonts-ttf alsa-plugins-pulseaudio neovim 
 
 
 ##### END #####
