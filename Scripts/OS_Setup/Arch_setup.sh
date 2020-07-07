a836671569ba77fa47b21f9c1553b1f72fa39f35
Gunpachi <37960846+Gunpachi@users.noreply.github.com>
2020-07-07 05:33:29 -0700

    v0.1 added some stuff
    Still in beta tho

--- Scripts/OS_Setup/Arch_setup.sh
+++ Scripts/OS_Setup/Arch_setup.sh
@@ -4,16 +4,17 @@
 #Update the system
 sudo pacman -Syyu
 
-#installing pikaur
-
+#installing yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 #Installing necessary apps
-
-
-
-
+cd $HOME
+yay -Sy xorg xorg-server gvfs  picom-ibhagwan-git polkit-gnome python python3-pip nitrogen openbox obconf tint2 fish Thunar thunar-volman firefox spotify alacritty 
 
 #Git clone the dotfiles repo
-
+cd $HOME/Documents/za-dots/
+cp -r config/* ~/.config/
 #copying WM configs
 
 #copying other app configs
