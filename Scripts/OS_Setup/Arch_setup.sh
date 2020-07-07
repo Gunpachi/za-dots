0ecac875b9835916d15ee6b929b82f920a07dcd0
Gunpachi <37960846+Gunpachi@users.noreply.github.com>
2020-07-07 18:18:58 +0530

    Update Arch_setup.sh

--- Scripts/OS_Setup/Arch_setup.sh
+++ Scripts/OS_Setup/Arch_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-
+mkdir Documents Downloads Videos Music .config .themes .icons 
+touch ~/.xinitrc
+cp $HOME/za-dots/xinitrc ~/.xinitrc
 #Update the system
 sudo pacman -Syyu
 
@@ -10,10 +12,10 @@ cd yay
 makepkg -si
 #Installing necessary apps
 cd $HOME
-yay -Sy xorg xorg-server gvfs  picom-ibhagwan-git polkit-gnome python python3-pip nitrogen openbox obconf tint2 fish Thunar thunar-volman firefox spotify alacritty 
+yay -Sy xorg xorg-server gvfs rofi-git lxappearance lightdm lightdm-webkit2-greeter picom-ibhagwan-git polkit-gnome python python3-pip nitrogen openbox obconf tint2 fish Thunar thunar-volman firefox spotify alacritty 
 
 #Git clone the dotfiles repo
-cd $HOME/Documents/za-dots/
+cd $HOME/za-dots/
 cp -r config/* ~/.config/
 #copying WM configs
 
